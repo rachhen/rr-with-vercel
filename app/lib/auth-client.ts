09d214d99bc906d4b475af16175b5260b8fcbc5b
Rachhen <rachhen.it@outlook.com>
2025-01-17 01:35:57 +0700

    update auth client

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const client = createAuthClient({
-  baseURL: "http://localhost:3000", // the base url of your auth server
+  // baseURL: "http://localhost:3000", // the base url of your auth server
 });
